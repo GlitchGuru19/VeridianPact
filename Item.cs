aab3e445d0b41c438be30087d9986602c8252878
GlitchGuru19 <peterkatebe12@gmail.com>
2025-05-09 22:58:52 +0200

    Added an Item class to be more of an Inventory, Inventory Object to be created soon in a different class though

---
+++ Item.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace VeridianPact
+{
+    class Item
+    {
+        public string? Name {get; private set; };
+        public string? Description { get; private set; };
+        public bool isUsable { get; private set; };
+
+        // Initializes an item with a name, description, and usability flag
+        public Item(string name, string description, bool isUsable = true)
+        {
+            Name = name;
+            Description = description;
+            IsUsable = isUsable;
+        }
+
+        // Use the item displaying a basic usage message
+        pulic virtual void Use(Player player, GameState gameState)
+        {
+            Console.WriteLine($"You use the {Name}.");
+        }
+    }
+}
