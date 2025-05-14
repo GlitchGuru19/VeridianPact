8bd18fa1e2410f6152d35898f233e4d25569a5db
GlitchGuru19 <peterkatebe12@gmail.com>
2025-05-14 22:46:44 +0200

    Added NPC class

---
+++ NPC.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace VeridiansPact
+{
+    class NPC
+    {
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public int RelationshipValue { get; private set; }
+
+        // Initializes an NPC with a name and description
+        public NPC(string name, string description)
+        {
+            Name = name;
+            Description = description;
+            RelationshipValue = 0;
+        }
+
+        // Modifies the relationship value with the NPC
+        public void ModifyRelationship(int amount)
+        {
+            RelationshipValue += amount;
+        }
+
+        // Handles interaction with the player, displaying a basic message
+        public virtual void Interact(Player player)
+        {
+            Console.WriteLine($"You approach {Name}.");
+        }
+    }
+}
--- OpeningScene.cs
+++ OpeningScene.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+// using VeridianPact; maybe..
 using Scene;
 
 namespace VeridiansPact
